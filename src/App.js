1743bb7f5aecaa40216b9f4413755b414de7ac86
NabeelAltaf <nabeelaltaf1@outlook.com>
2022-01-08 20:08:49 -0500

    Compilation Error Fix
    Compilation error on surge auto deployment fix attempt.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Room from './Room';
 
 function App () { // An object is coming from the props under the tag name which is equal to what is set in index
-
-  const [isLit, setLit] = useState(true);
 
   return (
 
